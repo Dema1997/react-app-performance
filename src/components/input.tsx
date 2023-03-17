b13a0e4da477944933864cd5ecd77eb44c10e85a
Filippo De marco <filippodemarco@MacBook-Pro-di-Filippo.local>
2023-03-17 14:32:17 +0100

    Fixes

---
+++ src/components/input.tsx
@@ -0,0 +1,42 @@
+import { Profiler } from "react";
+
+interface InputProps {
+  placeholder: string;
+  value: string;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
+}
+
+const Input = ({ placeholder, value, onChange }: InputProps) => {
+  return (
+    <Profiler
+      id="InputComponent"
+      onRender={(
+        id,
+        phase,
+        actualDuration,
+        baseDuration,
+        startTime,
+        commitTime,
+        interactions
+      ) => {
+        console.log("Profiler: id", id);
+        console.log("Profiler: phase", phase);
+        console.log("Profiler: actualDuration", actualDuration);
+        console.log("Profiler: baseDuration", baseDuration);
+        console.log("Profiler: startTime", startTime);
+        console.log("Profiler: commitTime", commitTime);
+        console.log("Profiler: interactions", interactions);
+        console.log("Profiler: Input Component");
+        console.log("------------------------------------------");
+      }}
+    >
+      <input
+        onChange={(e) => onChange(e)}
+        value={value}
+        placeholder={placeholder}
+      />
+    </Profiler>
+  );
+};
+
+export { Input };
